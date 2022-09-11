376a2d68b9639fa5686cdaeacac8c85c959e5ab1
zeddicus-pl <79473394+zeddicus-pl@users.noreply.github.com>
2022-09-11 20:28:07 +0200

    Delete src directory

--- src/stream/stream.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import StreamApp from "./StreamApp";
-import '../i18n';
-
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<StreamApp />);
--- src/utils/defaultSettings.ts
+++
@@ -1,8 +0,0 @@
-import { Settings } from "../@types/main.d";
-
-const defaultSettings: Settings = {
-  saveDir: '',
-  lang: '',
-}
-
-export default defaultSettings;
